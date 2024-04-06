02aba9153279f774650148bfb9a4d7cfd7cd21ac
lienvanduc2k2 <156681636+lienvanduc2k2@users.noreply.github.com>
2024-04-06 18:09:54 +0700

    feat: add footer (dev)

--- src/components/common/footer/index.tsx
+++ src/components/common/footer/index.tsx
@@ -1,5 +1,57 @@
+'use client'
+import eclipse from '@/icons/filters/ellipse.svg'
+import teleIcon from '@/icons/footer/telegram.svg'
+import xIcon from '@/icons/footer/x.svg'
+import Image from 'next/image'
+import { useState } from 'react'
 const Footer = () => {
-  return <div>Footer</div>
+  const [isActiveLite, setIsActiveLite] = useState(false)
+  const [isActiveShow, setIsActiveShow] = useState(false)
+
+  const toggleSwitchLite = () => {
+    setIsActiveLite(!isActiveLite)
+  }
+  const toggleSwitchShow = () => {
+    setIsActiveShow(!isActiveShow)
+  }
+  return (
+    <div className='flex h-8 items-center justify-between px-[100px] py-4'>
+      <div className='flex items-center gap-10'>
+        <div className='flex items-center gap-5'>
+          <div className='text-[14px] font-medium  leading-[14px] text-white'>Lite</div>
+          <button
+            className='flex h-[20px] w-[36.67px] items-center rounded-[20px] bg-[#FFFFFF1A] '
+            onClick={toggleSwitchLite}
+          >
+            <div className={`ml-[2px]  ${isActiveLite ? 'translate-x-[16px] transform' : ''}`}>
+              <Image src={eclipse} alt='switch_icon' />
+            </div>
+          </button>
+          <div className='text-[14px] font-medium  leading-[14px] text-white'>Pro</div>
+        </div>
+        <div className='h-[20px]  w-0 border border-solid border-[#FFFFFF3D]'></div>
+        <div className='flex items-center gap-5'>
+          <button
+            className='flex h-[20px] w-[36.67px] items-center rounded-[20px] bg-[#FFFFFF1A] '
+            onClick={toggleSwitchShow}
+          >
+            <div className={`ml-[2px]  ${isActiveShow ? 'translate-x-[16px] transform' : ''}`}>
+              <Image src={eclipse} alt='switch_icon' />
+            </div>
+          </button>
+          <div className='text-[14px] font-medium  leading-[14px] text-white'>Show $</div>
+        </div>
+      </div>
+      <div className='flex items-center gap-4'>
+        <button>
+          <Image src={xIcon} alt='twitter icon' />
+        </button>
+        <button>
+          <Image src={teleIcon} alt='telegram icon' />
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Footer
