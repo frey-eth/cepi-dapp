9eaa0baac10dbc4b8b65bc872616c3d5e2b11856
tinnd <tinndev21@gmail.com>
2024-04-06 18:09:30 +0700

    fix: fix button connect (dev)

---
+++ src/components/common/button/btnConnect.tsx
@@ -0,0 +1,5 @@
+const BtnConnect = () => {
+  return <button className='rounded-lg bg-gradient-connect px-4 py-2 text-base text-[#fff]'>Connect Wallet</button>
+}
+
+export default BtnConnect
--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -1,5 +1,61 @@
-const Header = () => {
-  return <div>Header</div>
-}
+'use client'
+import logo from '@/images/header/Logo.png'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import BtnCollectReward from '../button/btn-collect-reward'
+import BtnConnect from '../button/btnConnect'
 
+const navbars = [
+  {
+    id: 1,
+    title: 'Dashboard',
+    href: '/',
+  },
+  {
+    id: 2,
+    title: 'Loans',
+    href: '/loans',
+  },
+  {
+    id: 3,
+    title: 'Lotteries',
+    href: '/lotteries',
+  },
+]
+
+const Header = () => {
+  const pathname = usePathname()
+
+  return (
+    <main>
+      <section className='mx-auto flex w-full items-center justify-between py-4 lg:max-w-[1440px] lg:px-[100px]'>
+        <section className='flex w-[752px] items-center gap-x-10'>
+          <Link href={'/'}>
+            <figure>
+              <Image src={logo} alt='logo' />
+            </figure>
+          </Link>
+
+          <nav className='flex items-center space-x-4'>
+            {navbars.map((item, index) => {
+              return (
+                <Link href={item.href} key={index} className='group relative px-4 py-2'>
+                  <span className='text-base font-medium text-[#FFFFFF]'>{item.title}</span>
+                  <span
+                    className={`absolute bottom-0 left-0 block h-[2px] ${item.href !== pathname ? 'w-0 group-hover:w-full' : 'w-full'} bg-[#ED048A] transition-all duration-150 ease-linear `}
+                  ></span>
+                </Link>
+              )
+            })}
+          </nav>
+        </section>
+        <section className='flex items-center space-x-4'>
+          <BtnCollectReward />
+          <BtnConnect />
+        </section>
+      </section>
+    </main>
+  )
+}
 export default Header
