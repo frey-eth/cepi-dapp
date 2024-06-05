3e4cdc5547e18fdf2c89e886628be7b153829202
lienvanduc2k2 <156681636+lienvanduc2k2@users.noreply.github.com>
2024-06-05 11:52:28 +0700

    fix: update link x blonkfi (dev)

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,17 +1,18 @@
 import telegramIcon from '@/icons/footer/telegram.svg'
 import xIcon from '@/icons/footer/x.svg'
 import Image from 'next/image'
+import Link from 'next/link'
 const Footer = () => {
   return (
     <div className='flex h-8 flex-wrap items-center justify-center gap-4 px-4 py-4  pb-10 max-sm:hidden lg:justify-between lg:gap-0 lg:px-[100px]'>
       <div className='flex items-center gap-4 lg:gap-10' />
       <div className='flex items-center gap-4'>
-        <button>
+        <Link href={`https://x.com/BlonkFi`}>
           <Image src={xIcon} alt='twitter icon' />
-        </button>
-        <button>
+        </Link>
+        <Link href={`#`}>
           <Image src={telegramIcon} alt='telegram icon' />
-        </button>
+        </Link>
       </div>
     </div>
   )
