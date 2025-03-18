56fce0f6ae7587149912a7f5e0874b3339847e4f
Frey <duongpham321dz@gmail.com>
2025-03-18 16:56:32 +0700

    feat: update service worker (dev)

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-import withPWAInit from '@ducanh2912/next-pwa'
-
-const withPWA = withPWAInit({
-  dest: 'public',
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  disable: false,
-  workboxOptions: {
-    disableDevLogs: true,
-  },
-})
 
 const nextConfig = {
   reactStrictMode: false,
@@ -20,4 +8,4 @@ const nextConfig = {
   },
 }
 
-export default withPWA(nextConfig)
+export default nextConfig
